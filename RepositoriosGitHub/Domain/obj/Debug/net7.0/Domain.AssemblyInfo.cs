05b87949d8224ffd9e4331f430063daf72c93289
João Girardi <jvitorstahl@hotmail.com>
2025-04-10 20:57:33 -0300

    Ignored

--- RepositoriosGitHub/Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
+++ RepositoriosGitHub/Domain/obj/Debug/net7.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+689e3769c5cb76a23e7733fd94e8f7648a5a0cbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540d2a5ba1ed5d2df8239a5096096e3539d433a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
